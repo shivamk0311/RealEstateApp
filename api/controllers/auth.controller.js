3e79c5443e7372815c7c18f3623082273dcdd9d3
Shivam Khokhani <sxk2125@mavs.uta.edu>
2026-01-27 15:22:18 -0600

    Added Delete Account functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -48,7 +48,7 @@ export const google = async (req, res, next) => {
             const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
             const newUser = new User({username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4), email: req.body.email, password: hashedPassword, avatar: req.body.photo});
             await newUser.save();
-            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+            const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET);
             const {password: pass, ...rest} = user._doc;
             res
                 .cookie('access_token', token, { httpOnly: true })
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useRef, useState } from "react";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from "../firebase";
-import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice.js";
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from "../redux/user/userSlice.js";
 
 export default function Profile() {
   const {currentUser, loading, error} = useSelector(state => state.user);
@@ -13,7 +13,7 @@ export default function Profile() {
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const dispatch = useDispatch();
-  console.log(formData);
+  // console.log(formData);
 
   useEffect(() => {
     if (file){
@@ -71,6 +71,24 @@ export default function Profile() {
     }
   }
 
+  const handleDelete = async () => {
+    try{
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, 
+      {
+        method: 'DELETE'
+      });
+      const data = await res.json();
+      if(data.success === false){
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    }catch(error){
+      dispatch(deleteUserFailure(error.message));
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold mt-7 text-center'>Profile</h1>
@@ -93,7 +111,7 @@ export default function Profile() {
         <button disabled={loading} className="bg-amber-950 text-white rounded-lg p-3 uppercase hover:opacity-90 disabled:opacity-80">{loading ? 'Loading...' : 'Update'}</button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span onClick={handleDelete} className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
       <p className="text-red-700 mt-5 text-center">{error ? error : ''}</p>
