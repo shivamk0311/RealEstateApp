641d57aca33d7bf8550043dd36059e5f415fd086
Shivam Khokhani <sxk2125@mavs.uta.edu>
2026-02-15 15:41:43 -0600

    Updated BASE_URL

--- client/utils/api.js
+++ client/utils/api.js
@@ -1,6 +1,9 @@
-const BASE_URL =
-  import.meta.env.VITE_API_URL ||
-  (import.meta.env.DEV ? "http://localhost:3000" : "http://backend:3000");
+// const BASE_URL =
+//   import.meta.env.VITE_API_URL ||
+//   (import.meta.env.DEV ? "http://localhost:3000" : "http://backend:3000");
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 
 export const apiFetch = (endpoint, options = {}) => {
   return fetch(`${BASE_URL}${endpoint}`, {
